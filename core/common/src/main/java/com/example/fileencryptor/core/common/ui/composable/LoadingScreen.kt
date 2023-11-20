8172585c7c236e82f129dd4d6558db42a0e019d4
borednaeem <ahmed.m.naeem@outlook.com>
2023-11-20 13:12:46 +0200

    - add loading screen composable

---
+++ core/common/src/main/java/com/example/fileencryptor/core/common/ui/composable/LoadingScreen.kt
@@ -0,0 +1,37 @@
+package com.example.fileencryptor.core.common.ui.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            color = Color.White
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LoadingScreenPreview() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LoadingScreen()
+    }
+}
