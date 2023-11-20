a17f5d9d699a07cc4d4211e28e354d39be2c6a7f
borednaeem <ahmed.m.naeem@outlook.com>
2023-11-20 13:14:07 +0200

    - remove unused commented lines

--- custom-build-plugins/convention/build.gradle.kts
+++ custom-build-plugins/convention/build.gradle.kts
@@ -1,9 +1,6 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-//    id("java-library")
     `kotlin-dsl`
-     //alias(libs.plugins.org.jetbrains.kotlin.jvm)
-//    alias(libs.plugins.)
 }
 
 java {
